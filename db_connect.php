90835a64a33c351c7f118effbc07670199ec2ad5
DTCraft <mcpeytwot6@gmail.com>
2019-05-25 21:57:05 +0300

    ttt

--- db_connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$db_server = 'sql207.epizy.com';
-$db_login = 'epiz_23926542';
-$db_pass = 'LHDeXfeksopSr';
-$db_name = 'epiz_23926542_flatron';
-
-$con = mysqli_connect($db_server, $db_login, $db_pass);
-mysqli_select_db($con, $db_name);
